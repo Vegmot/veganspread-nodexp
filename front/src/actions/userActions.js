1eb7c740391ba28a6f953eb921fe2961974570ad
Vegmot <anthony.yh.jang@gmail.com>
2021-05-17 10:11:47 -0700

    register user working: code was correct but it appears to have been some technical glitches

--- front/src/actions/userActions.js
+++ front/src/actions/userActions.js
@@ -49,8 +49,8 @@ export const login = (email, password) => async dispatch => {
   }
 }
 
-export const registerUser =
-  (fName, lName, dName, email, password) => async dispatch => {
+export const register =
+  (firstName, lastName, displayName, email, password) => async dispatch => {
     try {
       dispatch({ type: REGISTER_USER_REQUEST })
 
@@ -62,7 +62,7 @@ export const registerUser =
 
       const res = await axios.post(
         '/api/users/register',
-        { fName, lName, dName, email, password },
+        { firstName, lastName, displayName, email, password },
         config
       )
 
--- front/src/screens/auth/RegisterForm.js
+++ front/src/screens/auth/RegisterForm.js
@@ -5,53 +5,56 @@ import * as Yup from 'yup'
 import MyTextInput from '../../components/form/MyTextInput'
 import ModalWrapper from '../../components/modals/ModalWrapper'
 import { closeModal } from '../../components/modals/modalReducer'
-import { registerUser } from '../../actions/userActions'
+import { register } from '../../actions/userActions'
 import { Button, Label, Divider } from 'semantic-ui-react'
 
 const RegisterForm = () => {
   const dispatch = useDispatch()
 
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required('Please enter your first name'),
+    lastName: Yup.string().required('Please enter your last name'),
+    displayName: Yup.string().required('Please enter your preferred username'),
+    email: Yup.string().email().required('Please enter valid email address'),
+    password: Yup.string().required('Password is required'),
+    confirmPassword: Yup.string()
+      .when('password', {
+        is: val => (val && val.length > 0 ? true : false),
+        then: Yup.string().oneOf(
+          [Yup.ref('password')],
+          'Passwords do not match'
+        ),
+      })
+      .required(),
+  })
+
+  const dispatchRegister = (fn, ln, dn, em, pw) => {
+    dispatch(register(fn, ln, dn, em, pw))
+  }
+
   return (
     <>
       <ModalWrapper size='mini' header='Register'>
         <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          }}
-          validationSchema={Yup.object({
-            firstName: Yup.string().required('Please enter your first name'),
-            lastName: Yup.string().required('Please enter your last name'),
-            displayName: Yup.string().required(
-              'Please enter your preferred username'
-            ),
-            email: Yup.string()
-              .required('Please enter valid email address')
-              .email(),
-            password: Yup.string().required('Password is required'),
-            confirmPassword: Yup.string().when('password', {
-              is: val => (val && val.length > 0 ? true : false),
-              then: Yup.string().oneOf(
-                [Yup.ref('password')],
-                'Passwords do not match'
-              ),
-            }),
-          })}
+          initialValues={initialValues}
+          validationSchema={validationSchema}
           onSubmit={(values, { setSubmitting, setErrors }) => {
             try {
-              console.log(values)
-              dispatch(
-                registerUser(
-                  values.firstName,
-                  values.lastName,
-                  values.displayName,
-                  values.email,
-                  values.password
-                )
+              dispatchRegister(
+                values.firstName,
+                values.lastName,
+                values.displayName,
+                values.email,
+                values.password
               )
               setSubmitting(false)
               dispatch(closeModal())
@@ -61,7 +64,7 @@ const RegisterForm = () => {
             }
           }}
         >
-          {({ isSubmitting, isValid, dirty }) => (
+          {({ isSubmitting, isValid, dirty, errors }) => (
             <Form className='ui form'>
               <MyTextInput name='firstName' placeholder='First name' />
               <MyTextInput name='lastName' placeholder='Last name' />
